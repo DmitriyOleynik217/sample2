1b729be1a3762159aaba6614c3f5a39662249977
Dmitriy Oleynik <oda180598@gmail.com>
2021-06-24 17:36:11 +0300

    keep working!

---
+++ app/code/Shark/ModuleCheckout/view/frontend/web/js/aureatelabsValidationRule.js
@@ -0,0 +1,18 @@
+define([
+    'jquery',
+    'jquery/ui',
+    'jquery/validate',
+    'mage/translate'
+], function($){
+    'use strict';
+    return function() {
+        $.validator.addMethod(
+            "aureatelabsvalidationrule",
+            function(value, element) {
+                //Perform your operation here and return the result true/false.
+                return true/false;
+            },
+            $.mage.__("Your validation message.")
+        );
+    }
+});
